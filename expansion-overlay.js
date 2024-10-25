fd7d069a1498c54de63f5117a467fa369f6f7d21
Azriel <thenapo212@gmail.com>
2024-10-25 19:21:49 +0200

    Control valores de atributos y calculo HorizontalAlign

--- expansion-overlay.js
+++ expansion-overlay.js
@@ -1,15 +1,22 @@
 class ExpansionOverlay extends HTMLElement {
 
+  /** Un flag para que no se muestren los mensajes de error de componente sin inicializar */
+  loadBeforeAttributesInit = true;
+  loadTemplatePromise;
+
+  /**
+   * VALORES POR DEFECTO
+   */
+  /** string, Id de HTMLElement. Id del item desde donde se despliega el expansion-overlay */
+  expandFromItem = null;
+  /** string, Id de HTMLElement. Id del item que aparecerá en el expansion-overlay */
+  componentToExpand = null;
+  /** string, Id de HTMLElement. Id del item que causa la expasión, suele ser un botón. A este HTMLElement se le añade un addEventListener 'click'. */
+  expandTrigger = null;
   /** string, Opciones posibles: 'left' (default) | 'middle' | 'right'. Indica donde se alineará el expansion-overlay en el eje X, por ejemplo si se alinea 'left' el expansion-overlay se pegará a la izquierda y si necesita más espacio crecerá hacia la derecha. */
   horizontalAlign = 'left';
   /** string, Opciones posibles: 'top' | 'bottom' (default). Indica donde se alineará el expansion-overlay en el eje Y, por ejemplo si se alinea 'top' el expansion-overlay se colocará por encima del objeto y la animación de expandirse será de abajo hacia arriba. */
   verticalAlign = 'bottom';
-  /** string, Id de HTMLElement. Id del item desde donde se despliega el expansion-overlay */
-  expandFromItem = '';
-  /** string, Id de HTMLElement. Id del item que aparecerá en el expansion-overlay */
-  componentToExpand = '';
-  /** string, Id de HTMLElement. Id del item que causa la expasión, suele ser un botón. A este HTMLElement se le añade un addEventListener 'click'. */
-  expandTrigger = '';
   /** boolean, Opciones posibles: 'true' (default) | 'false'. Indica si el width del expansion-overlay será igual que el width del HTMLElement de expandFromItem */
   inheritParentWidth = true;
   /** string, valores posibles: '350px' | '150em' | '50%' | '200vw'. Indica un width específico para el expansion-overlay (si el parametro inheritParentWidth está 'true' este parametro se ignora). */
@@ -23,10 +30,12 @@ class ExpansionOverlay extends HTMLElement {
      * El modo close activa el encapsulamiento del shadow dom, inhabilitando el acceso al html y css desde un componente externo.
     */
     this.attachShadow({ mode: 'open' });
-    this.loadTemplates();
+    this.loadTemplatePromise = new Promise((resolve) => {
+      this.loadTemplates(resolve);
+    });
   }
 
-  async loadTemplates() {
+  async loadTemplates(loadTemplateResolve) {
     // Cargar el HTML y CSS
     const [templateContent, cssContent] = await Promise.all([
       this.loadTemplate(),
@@ -41,7 +50,7 @@ class ExpansionOverlay extends HTMLElement {
     styleElement.textContent = cssContent;
     this.shadowRoot.appendChild(styleElement);
 
-    this.createOverlay();
+    this.createOverlay(loadTemplateResolve);
   }
 
   // Función para cargar el template HTML
@@ -61,7 +70,7 @@ class ExpansionOverlay extends HTMLElement {
     return await response.text();
   }
 
-  createOverlay() {
+  createOverlay(loadTemplateResolve) {
     const originalElement = document.getElementById("password-composition"); // Obtenemos el elemento al que le vamos a crear el padre // todo - por parametro
     const newParent = document.createElement('div'); // Creamos el elemento padre: overlay-container, el responsable de la animación de despliegue
     newParent.id = 'overlayContainer';
@@ -71,7 +80,6 @@ class ExpansionOverlay extends HTMLElement {
       width: 100%;
       max-height: 0;
       overflow: hidden;
-      padding: 0 15px;
       background-color: #f9f9f9;
       transition: max-height 0.3s ease-out;
       box-shadow: 0 -2px 10px rgba(0, 0, 0, 0.1); /* Sombras hacia arriba */
@@ -82,7 +90,6 @@ class ExpansionOverlay extends HTMLElement {
       /* max-height: 100px; */
       z-index: 10000;
       width: 350px;
-      left: 0%;
     `; // todo - revisar estructura
 
     const originalElementParent = originalElement.parentNode; // Accedemos al padre original del elemento al que le vamos a crear el padre
@@ -91,6 +98,7 @@ class ExpansionOverlay extends HTMLElement {
     // set element as child of wrapper
     newParent.appendChild(originalElement); // A nuestro elemento padre le añadimos el elemento original como hijo
 
+    loadTemplateResolve();
     this.manageVisibility();
   }
 
@@ -103,7 +111,7 @@ class ExpansionOverlay extends HTMLElement {
     backdrop.addEventListener('click', function () {
       const overlayMaxHeight = document.getElementById('overlayContainer').style.maxHeight;
       document.getElementById('overlayContainer').style.maxHeight = overlayMaxHeight == '0px' ? '200px' : '0px'; // Despliega - oculta el overlay
-      console.log(document.getElementById('overlayContainer').style.maxHeight);
+      // console.log(document.getElementById('overlayContainer').style.maxHeight);
       backdrop.classList.toggle('activated'); // Muestra - oculta el backdrop
     });
 
@@ -116,27 +124,47 @@ class ExpansionOverlay extends HTMLElement {
   }
 
   /**
-   * Ciclo de vida del webcomponent. Se ejecuta al crearse un elemento del componente.
+   * Ciclo de vida del webcomponent. Se ejecuta al crearse el webcomponent y otra vez cuando se cargan por primera vez todos los atributos del attributeChangedCallback.
    */
   connectedCallback() {
+    // Evita que salgan los mensajes de error, ya que este método se ejecuta antes de que lleguen los atributos y después de que lleguen los atributos.
+    if (this.loadBeforeAttributesInit) {
+      this.loadBeforeAttributesInit = false;
+      return;
+    }
 
+    if (this.getAttribute('expand-from-item-id') == null && this.getAttribute('expand-from-item-class') == null) {
+      console.error('ERROR: Property expand-from-item has not been initialized correctly.');
+      return;
+    }
+    else if (this.getAttribute('expand-from-item-id') != null && this.getAttribute('expand-from-item-class') != null) {
+      console.warn('WARNING: Property expand-from-item-id and expand-from-item-class are mutually exclusive, expand-from-item-id will be used by default.');
+    }
+
+    if (this.getAttribute('component-to-expand-id') == null && this.getAttribute('component-to-expand-class') == null) {
+      console.error('ERROR: Property component-to-expand has not been initialized correctly.');
+      return;
+    }
+    else if (this.getAttribute('component-to-expand-id') != null && this.getAttribute('component-to-expand-class') != null) {
+      console.warn('WARNING: Property component-to-expand-id and component-to-expand-class are mutually exclusive, component-to-expand-id will be used by default.');
+    }
+
+    if (this.getAttribute('expand-trigger-id') == null && this.getAttribute('expand-trigger-class') == null) {
+      // console.error('ERROR: Property expand-trigger has not been initialized correctly.'); // todo - reactivar
+      // return; // todo - reactivar
+    }
+    else if (this.getAttribute('expand-trigger-id') != null && this.getAttribute('expand-trigger-class') != null) {
+      // console.warn('WARNING: Property expand-trigger-id and expand-trigger-class are mutually exclusive, expand-trigger-id will be used by default.'); // todo - reactivar
+    }
+
+    this.manageExpansionOverlay();
   }
-
-  /**
-   * horizontal-align (string - left, middle, right)
-   * vertical-align (string - top, bottom) (¿¿Se deberia hacer un calculo antes para ver si se podrá visualizar correctamente??)
-   * expandFromItem (string - con el id del item desde donde se despliega el expansion-overlay)
-   * componentToExpand (string - con el id del item que aparecerá en el expansion-overlay)
-   * expandTrigger (string - con el id del item que causa la expasión, suele ser un botón)
-   * inheritParentWidth (boolean - falso por defecto - indica si el expansion-overlay tendrá el width del padre)
-   * customWidth (string - ej: '350px' - indica el width del expansion-overlay, si inheritParentWidth este parametro se ignora)
-   */
 
   /**
    * Ciclo de vida del webcomponent. Es el método que crea el observable, aquí se determina que las propiedades (@Input) que tendra el webcomponent.
    */
   static get observedAttributes() {
-    return ['horizontal-align', 'vertical-align', 'expand-from-item', 'component-to-expand', 'expand-trigger', 'inherit-parent-width', 'custom-width'];
+    return ['expand-from-item-id', 'expand-from-item-class', 'component-to-expand-id', 'component-to-expand-class', 'expand-trigger-id', 'expand-trigger-class', 'horizontal-align', 'vertical-align', 'inherit-parent-width', 'custom-width'];
   }
 
   /**
@@ -148,39 +176,179 @@ class ExpansionOverlay extends HTMLElement {
       return;
     }
 
-    console.log('attributeChangedCallback');
+    // No viene ningún valor
+    if (newValue == undefined || newValue == null) {
+      console.error(`ERROR: value can not be null or undefined. Property: ${name} has not been initialized correctly.`);
+      return;
+    }
+
+    // El valor es un string pero viene vacío
+    if ((typeof newValue === 'string' || newValue instanceof String) && (newValue.trim().length == 0)) {
+      console.error(`ERROR: string value can not be empty. Property: ${name} has not been initialized correctly.`);
+      return;
+    }
+
     if (name === 'horizontal-align') {
-      console.log(`attributeChangedCallback ${name} - ${oldValue} - ${newValue}`);
+      this.horizontalAlign = newValue;
       return;
     }
 
     if (name === 'vertical-align') {
-      console.log(`attributeChangedCallback ${name} - ${oldValue} - ${newValue}`);
+      this.verticalAlign = newValue;
       return;
     }
 
-    if (name === 'expand-from-item') {
-      console.log(`attributeChangedCallback ${name} - ${oldValue} - ${newValue}`);
+    if (name === 'expand-from-item-id' || name === 'expand-from-item-class') {
+      this.expandFromItem = newValue;
       return;
     }
 
-    if (name === 'component-to-expand') {
-      console.log(`attributeChangedCallback ${name} - ${oldValue} - ${newValue}`);
+    if (name === 'component-to-expand-id' || name === 'component-to-expand-class') {
+      this.componentToExpand = newValue;
       return;
     }
 
-    if (name === 'expand-trigger') {
-      console.log(`attributeChangedCallback ${name} - ${oldValue} - ${newValue}`);
+    if (name === 'expand-trigger-id' || name === 'expand-trigger-class') {
+      this.expandTrigger = newValue;
       return;
     }
 
     if (name === 'inherit-parent-width') {
-      console.log(`attributeChangedCallback ${name} - ${oldValue} - ${newValue}`);
+      this.inheritParentWidth = newValue;
       return;
     }
 
     if (name === 'custom-width') {
-      console.log(`attributeChangedCallback ${name} - ${oldValue} - ${newValue}`);
+      this.customWidth = newValue;
+      return;
+    }
+  }
+
+  getExpandFromItem() {
+    let expandFromItem = null;
+
+    // Obtenemos el elemento mediante ID
+    if (this.getAttribute('expand-from-item-id')) {
+      expandFromItem = document.getElementById(this.getAttribute('expand-from-item-id'));
+      // No se ha encontrado HTMLElement
+      if (expandFromItem == null) {
+        console.error(`ERROR: Could not find HTMLElement with ID ${this.getAttribute('expand-from-item-id')}`);
+        return null;
+      }
+    }
+
+    // Obtenemos el elemento mediante clase
+    else if (this.getAttribute('expand-from-item-class')) {
+      expandFromItem = document.getElementsByClassName(this.getAttribute('expand-from-item-class'));
+      // No se ha encontrado HTMLElement
+      if (expandFromItem == null || expandFromItem.length == 0) {
+        console.error(`ERROR: Could not find HTMLElement with class name ${this.getAttribute('expand-from-item-class')}`);
+        return null;
+      }
+      // Se ha encontrado más de un HTMLElement
+      if (expandFromItem.length > 1) {
+        console.error(`ERROR: Found more than one HTMLElement with class name ${this.getAttribute('expand-from-item-class')}. Be more especific.`);
+        return null;
+      }
+      expandFromItem = expandFromItem.item(0);
+    }
+
+    return expandFromItem;
+  }
+
+  getComponentToExpand() {
+    let componentToExpand = null;
+
+    // Obtenemos el elemento mediante ID
+    if (this.getAttribute('component-to-expand-id')) {
+      componentToExpand = document.getElementById(this.getAttribute('component-to-expand-id'));
+      // No se ha encontrado HTMLElement
+      if (componentToExpand == null) {
+        console.error(`ERROR: Could not find HTMLElement with ID ${this.getAttribute('component-to-expand-id')}`);
+        return null;
+      }
+    }
+
+    // Obtenemos el elemento mediante clase
+    else if (this.getAttribute('component-to-expand-class')) {
+      componentToExpand = document.getElementsByClassName(this.getAttribute('component-to-expand-class'));
+      // No se ha encontrado HTMLElement
+      if (componentToExpand == null || componentToExpand.length == 0) {
+        console.error(`ERROR: Could not find HTMLElement with class name ${this.getAttribute('component-to-expand-class')}`);
+        return null;
+      }
+      // Se ha encontrado más de un HTMLElement
+      if (componentToExpand.length > 1) {
+        console.error(`ERROR: Found more than one HTMLElement with class name ${this.getAttribute('component-to-expand-class')}. Be more especific.`);
+        return null;
+      }
+      componentToExpand = componentToExpand.item(0);
+    }
+
+    return componentToExpand;
+  }
+
+  getExpandTrigger() {
+    let expandTrigger = null;
+
+    // Obtenemos el elemento mediante ID
+    if (this.getAttribute('expand-trigger-id')) {
+      expandTrigger = document.getElementById(this.getAttribute('expand-trigger-id'));
+      // No se ha encontrado HTMLElement
+      if (expandTrigger == null) {
+        console.error(`ERROR: Could not find HTMLElement with ID ${this.getAttribute('expand-trigger-id')}`);
+        return null;
+      }
+    }
+
+    // Obtenemos el elemento mediante clase
+    else if (this.getAttribute('expand-trigger-class')) {
+      expandTrigger = document.getElementsByClassName(this.getAttribute('expand-trigger-class'));
+      // No se ha encontrado HTMLElement
+      if (expandTrigger == null || expandTrigger.length == 0) {
+        console.error(`ERROR: Could not find HTMLElement with class name ${this.getAttribute('expand-trigger-class')}`);
+        return null;
+      }
+      // Se ha encontrado más de un HTMLElement
+      if (expandTrigger.length > 1) {
+        console.error(`ERROR: Found more than one HTMLElement with class name ${this.getAttribute('expand-trigger-class')}. Be more especific.`);
+        return null;
+      }
+      expandTrigger = expandTrigger.item(0);
+    }
+
+    return expandTrigger;
+  }
+
+  async manageExpansionOverlay() {
+    await this.loadTemplatePromise;
+    this.updateHorizontalAlign();
+  }
+
+  /**
+   * Actualiza
+   */
+  updateHorizontalAlign() { // TODO - HACER CALCULO, SI ESTÁ A LEFT POR EJEMPLO PERO NO HAY ESPACIO, PONERLO EN RIGHT Y VICEVERSA. SI ESTÁ EN MIDDLE Y NO HAY ESPACIO CAMBIAR TAMBIÉN. PERO SI CON LEFT POR EJEMPLO NO HAY ESPACIO NUNCA PONER MIDDLE. MIDDLE ES SOLO MANUAL.
+    let expandFromItem = this.getExpandFromItem();
+    if (expandFromItem == null) {
+      return;
+    }
+
+    const horizontalAlign = this.getAttribute('horizontal-align') ?? this.horizontalAlign;
+    if (horizontalAlign == 'left') {
+      document.getElementById('overlayContainer').style.left = `${expandFromItem.offsetLeft}px`;
+      return;
+    }
+
+    if (horizontalAlign == 'right') {
+      const offsetRight = window.innerWidth - expandFromItem.offsetLeft - expandFromItem.offsetWidth;
+      document.getElementById('overlayContainer').style.right = `${offsetRight}px`;
+      return;
+    }
+
+    if (horizontalAlign == 'middle') {
+      const offsetMiddle = (expandFromItem.clientWidth / 2) - (document.getElementById('overlayContainer').clientWidth / 2);
+      document.getElementById('overlayContainer').style.left = `${expandFromItem.offsetLeft + offsetMiddle}px`;
       return;
     }
   }
